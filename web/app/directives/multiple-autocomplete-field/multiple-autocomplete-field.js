87942df027b8a8322257fb6241fd75a7b70dfbbc
Faisal Nawaz <fslnwz0@gmail.com>
2016-07-14 15:36:33 -0400

    multi-autocomplete working state

---
+++ web/app/directives/multiple-autocomplete-field/multiple-autocomplete-field.js
@@ -0,0 +1,166 @@
+(function () {
+
+  angular.module('multipleSelect').directive('multipleAutocomplete', [
+    '$filter',
+    '$http',
+    function ($filter, $http) {
+      return {
+        restrict: 'EA',
+        scope: {
+          suggestionsArr: '=',
+          modelArr: '=ngModel',
+          apiUrl: '@',
+          disable: '=',
+          canSelect: '='
+        },
+        templateUrl: 'app/directives/multiple-autocomplete-field/multiple-autocomplete-tpl.html',
+        link: function (scope, element, attr) {
+          scope.objectProperty = attr.objectProperty;
+          scope.selectedItemIndex = 0;
+          scope.name = attr.name;
+          scope.isRequired = attr.required;
+          scope.errMsgRequired = attr.errMsgRequired;
+          scope.isHover = false;
+          scope.isFocused = false;
+          scope.showInput = true;
+          scope.showOptionList = true;
+          var getSuggestionsList = function () {
+            var url = scope.apiUrl;
+            console.log(url);
+            $http({
+              method: 'GET',
+              url: url
+            }).then(function (response) {
+              console.log(response);
+              scope.suggestionsArr = response.data;
+            }, function (response) {
+              console.log("*****Angular-multiple-select **** ----- Unable to fetch list");
+            });
+          };
+
+          if (scope.suggestionsArr == null || scope.suggestionsArr == "") {
+            if (scope.apiUrl != null && scope.apiUrl != "")
+              getSuggestionsList();
+            else {
+              console.log("*****Angular-multiple-select **** ----- Please provide suggestion array list or url");
+            }
+          }
+
+          if (scope.modelArr == null || scope.modelArr == "") {
+            scope.modelArr = [];
+          }
+          scope.onFocus = function () {
+            scope.isFocused = true
+          };
+
+          scope.onMouseEnter = function () {
+            scope.isHover = true
+          };
+
+          scope.onMouseLeave = function () {
+            scope.isHover = false;
+          };
+
+          scope.onBlur = function () {
+            scope.isFocused = false;
+          };
+
+          scope.onChange = function () {
+            scope.selectedItemIndex = 0;
+          };
+
+          scope.keyParser = function ($event) {
+            var keys = {
+              38: 'up',
+              40: 'down',
+              8: 'backspace',
+              13: 'enter',
+              9: 'tab',
+              27: 'esc'
+            };
+            var key = keys[$event.keyCode];
+            if (key == 'backspace' && scope.inputValue == "") {
+              if (scope.modelArr.length != 0)
+                scope.modelArr.pop();
+            } else if (key == 'down') {
+              var filteredSuggestionArr = $filter('filter')(scope.suggestionsArr, scope.inputValue);
+              filteredSuggestionArr = $filter('filter')(filteredSuggestionArr, scope.alreadyAddedValues);
+              if (scope.selectedItemIndex < filteredSuggestionArr.length - 1)
+                scope.selectedItemIndex++;
+            } else if (key == 'up' && scope.selectedItemIndex > 0) {
+              scope.selectedItemIndex--;
+            } else if (key == 'esc') {
+              scope.isHover = false;
+              scope.isFocused = false;
+            } else if (key == 'enter') {
+              var filteredSuggestionArr = $filter('filter')(scope.suggestionsArr, scope.inputValue);
+              filteredSuggestionArr = $filter('filter')(filteredSuggestionArr, scope.alreadyAddedValues);
+              if (scope.selectedItemIndex < filteredSuggestionArr.length)
+                scope.onSuggestedItemsClick(filteredSuggestionArr[scope.selectedItemIndex]);
+            }
+          };
+
+          scope.onSuggestedItemsClick = function (selectedValue) {
+            if (scope.canSelect != null && scope.canSelect != "") {
+              if (scope.modelArr.length < scope.canSelect) {
+                scope.modelArr.push(selectedValue);
+                scope.inputValue = "";
+              }
+            } else {
+              scope.modelArr.push(selectedValue);
+              scope.inputValue = "";
+            }
+            shouldShowInput();
+          };
+
+          var isDuplicate = function (arr, item) {
+            var duplicate = false;
+            if (arr == null || arr == "")
+              return duplicate;
+
+            for (var i = 0; i < arr.length; i++) {
+              duplicate = angular.equals(arr[i], item);
+              if (duplicate)
+                break;
+            }
+            return duplicate;
+          };
+
+          scope.alreadyAddedValues = function (item) {
+            var isAdded = true;
+            isAdded = !isDuplicate(scope.modelArr, item);
+            return isAdded;
+          };
+
+          scope.removeAddedValues = function (item) {
+            if (scope.modelArr != null && scope.modelArr != "") {
+              var itemIndex = scope.modelArr.indexOf(item);
+              if (itemIndex != -1)
+                scope.modelArr.splice(itemIndex, 1);
+            }
+            shouldShowInput();
+          };
+
+          function shouldShowInput() {
+            if (scope.canSelect != null && scope.canSelect != "") {
+              scope.showInput = scope.modelArr.length >= scope.canSelect ? false : true;
+              scope.showOptionList = scope.modelArr.length >= scope.canSelect ? false : true;
+            } else {
+              scope.showInput = scope.modelArr.length === scope.suggestionsArr.length ? false : true;
+              scope.showOptionList = scope.modelArr.length === scope.suggestionsArr.length ? false : true;
+            }
+          }
+
+          scope.clearAll = function () {
+            scope.modelArr = [];
+            shouldShowInput();
+          }
+
+          scope.mouseEnterOnItem = function (index) {
+            scope.selectedItemIndex = index;
+          };
+        }
+      };
+    }
+  ]);
+})();
